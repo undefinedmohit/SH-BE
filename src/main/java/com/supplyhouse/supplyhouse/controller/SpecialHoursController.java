6f5ad86c26fdc76f523313178d0e8274d106bfe2
Mohit <mohit24yadav24@gmail.com>
2024-09-08 21:09:37 +0530

    added cross origin which helpes FE to get connected

--- src/main/java/com/supplyhouse/supplyhouse/controller/SpecialHoursController.java
+++ src/main/java/com/supplyhouse/supplyhouse/controller/SpecialHoursController.java
@@ -3,6 +3,7 @@ package com.supplyhouse.supplyhouse.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.supplyhouse.supplyhouse.service.SpecialHoursService;
 
 @RestController
 @RequestMapping("/hours")
+@CrossOrigin("*")
 public class SpecialHoursController {
 
     @Autowired  
